37212510bc2e0fac97444da57d8f035bcc8ac2e2
1n-hasee <2143572870@qq.com>
2023-03-03 13:42:02 +0800

    first push

---
+++ CMakeFiles/main.dir/src/client.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/src/client.cpp.o: \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW2/src/client.cpp \
+ /usr/include/stdc-predef.h \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW2/include/client.h
---
+++ CMakeFiles/main.dir/src/server.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/src/server.cpp.o: \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW2/src/server.cpp \
+ /usr/include/stdc-predef.h \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW2/include/server.h
